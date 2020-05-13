e3eb10acd0f1d9b10b6b5dd85c2494dd22a90913
Chad Rehm <chad.rehm@kineticdata.com>
2020-05-13 13:17:46 -0500

    passing ref to siblings

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component, createRef } from 'react';
 import { compose, withHandlers } from 'recompose';
 import './App.css';
 import { Calendar } from './Calendar';
 
-const AppComponent = ({}) =>
-  <div className="App">
-    <div className="calendar_wrapper">
-      <Calendar />
-    </div>
-  </div>
+class AppComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.calendarRef = createRef();
+  }
 
+  clickhandler = () => {
+    this.calendarRef.current.handleClick()
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.clickhandler}>Click Me</button>
+        <div className="calendar_wrapper">
+          <Calendar ref={this.calendarRef} />
+        </div>
+      </div>
+    )
+  }
+}
 
 
 export const App = compose(
--- src/Calendar.js
+++ src/Calendar.js
@@ -1,52 +1,50 @@
-import React, { useRef, Fragment, useMemo } from 'react';
+import React, { Component, Fragment, createRef } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
-import { compose, withHandlers } from 'recompose';
 import moment from 'moment';
 
 import './main.scss';
 import { events } from './events';
 
-export const handleClick = fullcalendar => event => {
-  const api = fullcalendar.current.getApi();
-  api.gotoDate(moment().subtract(1, 'months').format());
-}
+export class Calendar extends Component {
+  constructor(props) {
+    super(props);
+    this.calendarRef = createRef();
+  }
 
-export const CalendarComponent = ({handleEventClick}) => {
-  const apiRef = useRef(null);  
-  const clickhandler = useMemo(() => handleClick(apiRef), [apiRef]);
+  handleEventClick = ({ event }) => {
+    console.log(event.extendedProps.details)
+  }
 
-  return ( 
-    <Fragment>
-      <button onClick={clickhandler}>Click Me</button>
-      <FullCalendar 
-        events={events}
-        ref={apiRef}
-        height={"parent"}
-        contentHeight={"auto"}
-        eventLimit={3}
-        defaultView="dayGridDay" 
-        plugins={[ dayGridPlugin, timeGridPlugin ]} 
-        header={{
-          left: 'prev,next today',
-          center: 'title',
-          right: 'dayGridMonth,dayGridWeek,dayGridDay,timeGridDay'
-        }}
-        buttonText={{
-          timeGridDay: 'agenda'
-        }}
-        eventClick={handleEventClick}
-        fixedWeekCount={false}
-      />
-    </Fragment>
-  )
+  handleClick = event => {
+    const api = this.calendarRef.current.getApi();
+    api.gotoDate(moment().subtract(1, 'months').format());
+  }
+
+  render() {
+    return ( 
+      <Fragment>
+        <FullCalendar 
+          events={events}
+          ref={this.calendarRef}
+          height={"parent"}
+          contentHeight={"auto"}
+          eventLimit={3}
+          defaultView="dayGridDay" 
+          plugins={[ dayGridPlugin, timeGridPlugin ]} 
+          header={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,dayGridWeek,dayGridDay,timeGridDay'
+          }}
+          buttonText={{
+            timeGridDay: 'agenda'
+          }}
+          eventClick={this.handleEventClick}
+          fixedWeekCount={false}
+        />
+      </Fragment>
+    )
+  }
 };
-
-const handleEventClick = () => ({event}) => {
-  console.log(event.extendedProps.details)
-}
-
-export const Calendar = compose(
-  withHandlers({handleEventClick})
-)(CalendarComponent)
